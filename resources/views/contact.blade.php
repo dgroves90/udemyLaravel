5621533cd1748c9fe95194a7da512052e1552ad8
Danny <d.v.groves@hotmail.co.uk>
2019-10-30 08:09:48 +0000

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout');
+
+@section('content')
+    <h1>Contact</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum dolor exercitationem ab nemo aliquid omnis. Rem, placeat debitis perferendis enim, id consequuntur veniam impedit unde fugiat, eius laudantium error minima blanditiis aperiam corrupti ipsum commodi explicabo iste cupiditate! Ab suscipit sapiente nulla ipsam aut totam velit provident excepturi, molestias distinctio.</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout');
+
+@section('content')
+    <h1>Welcome</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt laudantium quibusdam fuga, eligendi saepe, quae fugit aut asperiores quas repellendus suscipit iure sapiente sed aspernatur eos aliquid quia illum maiores dicta, esse dolorem? Nobis nulla non maxime voluptates rem quas facere, excepturi minima tenetur tempora cum suscipit nisi totam odio sequi provident dolor hic natus repellendus distinctio, optio similique aperiam rerum. Veniam magnam esse ducimus sunt ex, similique dicta nemo provident, maxime velit voluptatem reiciendis nisi. Repellat, natus velit inventore neque rerum enim doloremque quas delectus eaque quam dignissimos perferendis magnam, debitis eos quibusdam in consequatur dolore eius nesciunt nam!</p>
+@endsection
